6f455f5b645604751ceb04b148d60afd2e717abc
seekwe <seekwe@gmail.com>
2018-06-20 17:44:10 +0800

    添加自身更新

--- src/Update.php
+++ src/Update.php
@@ -15,8 +15,8 @@ use Z;
  */
 class Update extends Command
 {
-    const LOG_URL = 'http://localhost:5000/update.json';
-    const FRAMEWORK_URL = 'http://localhost:5000/application/core/Zls.php';
+    const LOG_URL = 'http://zlsphp.tx.73zls.com/update.json';
+    const FRAMEWORK_URL = 'https://raw.githubusercontent.com/sohaha/ZlsPHP/master/application/core/Zls.php';
 
     public function description()
     {
@@ -30,7 +30,10 @@ class Update extends Command
 
     public function handle()
     {
-        return [];
+        return [
+            'self' => 'Update Framework',
+            'log'  => 'Check the update log, do not update',
+        ];
     }
 
     public function example()
@@ -38,41 +41,51 @@ class Update extends Command
         return [];
     }
 
-
-    public function execute($args)
+    public function self()
     {
-        //获取当前版本与远程对比
+        $this->log(true);
+    }
+
+    public function log($download = false)
+    {
         $version = IN_ZLS;
         /**
-         * @var \Zls\Action\Http $Action \Http
+         * @var \Zls\Action\Http $ActionHttp
          */
         $ActionHttp = z::extension('Action\Http');
-        $updateLog = $ActionHttp->get(self::LOG_URL);
-        $updateLog = @json_decode($updateLog, true);
+        $updateLog = $ActionHttp->get(self::LOG_URL, null, null, 1);
+        $updateLog = @json_decode($updateLog, true) ?: [];
         $updateLog = z::arrayFilter($updateLog, function ($e, $k) use ($version) {
             return $k > $version;
         });
-        if($updateLog){
+        if ($updateLog) {
+            $this->download();
             $log = ['Update Log' . PHP_EOL];
             foreach ($updateLog as $v => $t) {
-                $log[] = $v . ":\n" . $t . PHP_EOL;
+                $this->printStrN("v{$v}:", 'white');
+                $this->printStrN($t, 'light_gray');
+                $this->printStrN();
             }
-            $this->printStrN(join("\n", $log), 'green');
-        }else{
-            $this->printStrN(join("\n", $log), 'green');
+        } else {
+            $this->success('Already the latest version, No need to update.', 'green');
         }
-
     }
 
-    public function download()
+    protected function download()
     {
-        //FRAMEWORK_URL
         /**
-         * @var \Zls\Action\Http $Action \Http
+         * @var \Zls\Action\Http $ActionHttp
          */
         $ActionHttp = z::extension('Action\Http');
-        $content = $ActionHttp->get(self::FRAMEWORK_URL);
-        z::dump($content);
-        z::finish();
+        $content = $ActionHttp->get(self::FRAMEWORK_URL, null, null, 1);
+        $savePath = z::tempPath() . '/Zls.php';
+        if (strlen($content) > 500 && @file_put_contents($savePath, $content)) {
+            rename($savePath, ZLS_FRAMEWORK);
+        }
+    }
+
+    public function execute($args)
+    {
+        return $this->help($args);
     }
 }
