910d5b6de7ba84195d70bc5f68421e02d23398c6
seekwe <soseek@vip.qq.com>
2018-11-15 17:46:39 +0800

    -

--- src/Command.php
+++ src/Command.php
@@ -6,7 +6,6 @@ use Z;
 
 /**
  * Command.
- *
  * @author        影浅-Seekwe
  * @email         seekwe@gmail.com
  * @updatetime    2018-7-22 13:37:09
@@ -25,23 +24,24 @@ abstract class Command
             $handles = $this->getHandle();
         }
         $commandPrefix = z::arrayGet($args, 0);
-        $commandStr    = $commandPrefix.' '.$this->color($command);
-        $usage         = '';
-        $usage         .= $handles ? $this->color(':{command}', 'cyan') : '';
-        $usage         .= $options ? $this->color(' [options ...]', 'dark_gray')
+        $commandStr = $commandPrefix . ' ' . $this->color($command);
+        $usage = '';
+        $usage .= $handles ? $this->color(':{command}', 'cyan') : '';
+        $usage .= $options ? $this->color(' [options ...]', 'dark_gray')
             : '';
-        $example       = static::example();
+        $example = static::example();
         $this->printStrN(static::description(), 'light_green');
-        //$this->printStrN();
-        //$this->printStrN('Usage:', 'yellow');
-        //$this->printStrN('  '.$commandStr.$usage);
+        $commandOptions = [];
         if ($handles) {
             $this->printStrN();
             $this->printStrN('Available commands:', 'yellow');
-            foreach (
-                $this->beautify($handles, $command, 'cyan', ':') as $k => $v
-            ) {
-                $this->printStrN('  '.$commandPrefix.' '.ltrim($v));
+            $handles = $this->beautify($handles, $command, 'cyan', ':', $commandOptions);
+            foreach ($handles as $k => $v) {
+                //if (is_string($v)) {
+                $this->printStrN('  ' . $commandPrefix . ' ' . ltrim($v));
+                //} else {
+                //$commandOptions = ;
+                //}
             }
         }
         if ($options) {
@@ -51,12 +51,22 @@ abstract class Command
                 $this->printStrN($v);
             }
         }
+        if (!!$commandOptions) {
+            $this->printStrN();
+            $this->printStrN('Commands options:', 'yellow');
+            foreach ($commandOptions as $k => $v) {
+                $this->printStrN('  ' . $k . '', 'purple');
+                foreach ($this->beautify($v, '', 'dark_gray') as $kk => $vv) {
+                    $this->printStrN(' ' . $vv);
+                }
+            }
+        }
         if ($example) {
             $this->printStrN();
             $this->printStrN('Example:', 'yellow', '');
             foreach (
-                $this->beautify($example, z::arrayGet($args, 0).' '.$command,
-                    'cyan', false) as $k => $v
+                $this->beautify($example, z::arrayGet($args, 0) . ' ' . $command,
+                                'cyan', false) as $k => $v
             ) {
                 $this->printStrN($v);
             }
@@ -65,14 +75,12 @@ abstract class Command
 
     /**
      * 命令配置.
-     *
      * @return array
      */
     abstract public function options();
 
     /**
      * 子命令.
-     *
      * @return array
      */
     public function commands()
@@ -82,14 +90,13 @@ abstract class Command
     final public function getHandle()
     {
         $keys = array_diff(get_class_methods($this),
-            get_class_methods(__CLASS__));
+                           get_class_methods(__CLASS__));
 
         return array_fill_keys($keys, '');
     }
 
     /**
      * 命令示例.
-     *
      * @return array
      */
     public function example()
@@ -99,18 +106,13 @@ abstract class Command
 
     /**
      * 命令介绍.
-     *
      * @return string
      */
     abstract public function description();
 
-    final public function beautify(
-        $commands,
-        $command = '',
-        $color = '',
-        $pre = ' '
-    ) {
-        $lists   = [];
+    final public function beautify($commands, $command = '', $color = '', $pre = ' ', &$deploy = null)
+    {
+        $lists = [];
         $isAssoc = function ($array) {
             if (is_array($array)) {
                 $keys = array_keys($array);
@@ -120,9 +122,9 @@ abstract class Command
 
             return false;
         };
-        $is      = $isAssoc($commands);
-        $maxLen  = 10;
-        $_tmp    = array_keys($commands);
+        $is = $isAssoc($commands);
+        $maxLen = 10;
+        $_tmp = array_keys($commands);
         usort($_tmp, function ($e, $c) {
             return strlen($e) < strlen($c);
         });
@@ -131,16 +133,23 @@ abstract class Command
             $len = $maxLen;
         }
         foreach ($commands as $key => $value) {
-            $m = $this->color(str_pad($key, $len), $color);
+            $cmd = str_pad($key, $len);
             if (!z::strBeginsWith($key, ' ') && !z::strBeginsWith($key, ':')) {
                 if (false === $pre) {
                     $command = '';
                 } else {
-                    $m = $pre.$m;
+                    $cmd = $pre . $cmd;
                 }
             }
-            $lists[] = !$is ? '  '.$command.$value
-                : '  '.$command.$m.'  '.$value;
+            $m = $this->color($cmd, $color);
+            if (is_array($value)) {
+                if ($m) {
+                    $k = trim($cmd);
+                    $deploy [trim(z::strBeginsWith($k, ':') ? $command . $k : $k)] = $value[1];
+                }
+                $value = $value[0];
+            }
+            $lists[] = !$is ? '  ' . $command . $value : '  ' . $command . $m . '  ' . $value;
         }
 
         return $lists;
@@ -148,9 +157,7 @@ abstract class Command
 
     /**
      * 命令默认执行.
-     *
      * @param $args
-     *
      * @return mixed
      */
     abstract public function execute($args);
